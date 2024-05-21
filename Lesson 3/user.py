28d554c18b3cda60a5e275d057c26dd701504e84
Alexander Zuikov <sasha.ekberg@gmail.com>
2024-05-21 11:51:49 +0300

    Поправки по ДЗ №3

--- Lesson 3/user.py
+++ Lesson 3/user.py
@@ -1,6 +1,13 @@
-from lesson_3_task_1 import User 
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
 
-my_user = User("Александр", "Зуйков")
-my_user.print_first_name()
-my_user.print_last_name()
-my_user.print_full_name()
+    def print_first_name(self):
+        print(self.first_name)
+
+    def print_last_name(self):
+        print(self.last_name)
+
+    def print_full_name(self):
+        print(self.first_name, self.last_name)
